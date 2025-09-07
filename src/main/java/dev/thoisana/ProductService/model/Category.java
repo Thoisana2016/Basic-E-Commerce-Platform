6a69260fdf3624e9ffd981809f45eb7aa210fe63
Thoisana <thoisana.keisham@infosys.com>
2025-09-07 10:28:20 +0530

    remove constructor from models

--- src/main/java/dev/thoisana/ProductService/model/Category.java
+++ src/main/java/dev/thoisana/ProductService/model/Category.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Category {
     private Long id;
     private String title;
--- src/main/java/dev/thoisana/ProductService/model/Product.java
+++ src/main/java/dev/thoisana/ProductService/model/Product.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Product {
     private Long id;
     private String title;
